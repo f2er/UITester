a1456394159fc65f346ccf15b305fd326bf065d1
bofei <zhang.quan.109@gmail.com>
2012-06-13 13:44:29 +0800

    update

--- proxy/inject-record.php
+++ proxy/inject-record.php
@@ -1,23 +1,24 @@
 <!--inject-proxy-for-record-->
 
-<script src="./postmsg.js"></script>
-<script src="../../lib/kissy-aio.js"></script>
-<script src="../../lib/jasmine.js"></script>
-<script src="../../lib/jasmine-html.js"></script>
-<script src="../../lib/event-simulate.js"></script>
-<script src="../../lib/matcher.js"></script>
-<script src="./core.js" charset="utf-8"></script>
+
+<script src="http://uitest.taobao.net/UITester/lib/kissy-aio.js"></script>
+<script src="http://uitest.taobao.net/UITester/lib/jasmine.js"></script>
+<script src="http://uitest.taobao.net/UITester/lib/jasmine-html.js"></script>
+<script src="http://uitest.taobao.net/UITester/lib/event-simulate.js"></script>
+<script src="http://uitest.taobao.net/UITester/lib/matcher.js"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/postmsg.js"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/core.js" charset="utf-8"></script>
 <script>
     uitest.inner.init();
 </script>
-<script src="./events.js" charset="utf-8"></script>
-<script src="./attrs.js" charset="utf-8"></script>
-<script src="./tags.js" charset="utf-8"></script>
-<script src="./position.js" charset="utf-8"></script>
-<script src="./center.js" charset="utf-8"></script>
-<script src="./style.js" charset="utf-8"></script>
-<script src="./innerhtml.js" charset="utf-8"></script>
-<script src="./subtree.js" charset="utf-8"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/events.js" charset="utf-8"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/attrs.js" charset="utf-8"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/tags.js" charset="utf-8"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/position.js" charset="utf-8"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/center.js" charset="utf-8"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/style.js" charset="utf-8"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/innerhtml.js" charset="utf-8"></script>
+<script src="http://uitest.taobao.net/UITester/tool/record/subtree.js" charset="utf-8"></script>
 
 <?php echo $placeholder ?>
 
