6a7bf4305dd25d295d83242308cd5b0cbd4ac9e8
bofei <zhang.quan.109@gmail.com>
2012-06-18 16:33:55 +0800

    update

--- tool/update_result.php
+++ tool/update_result.php
@@ -10,13 +10,13 @@ $total_specs = trim($_REQUEST['total_specs']);
 
 // modify current record
 
-$sql = "UPDATE `list` SET `failed_specs`=".$failed_specs.",`total_specs`="+$total_specs+" WHERE id=".$task_id;
+$sql = "UPDATE `list` SET `failed_specs`=".$failed_specs.",`total_specs`=".$total_specs." WHERE id=".$task_id;
 
 
 $result = mysql_query($sql);
 $resultString  = $result === true ? "200" : "404";
-echo $sql;
-echo $result;
+//echo $sql;
+//echo $result;
 echo ('{"code" : ' . $resultString . '}');
 
 
