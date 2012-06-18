46295e60a0cfccab270ba871dbe1c4134ac62119
bofei <zhang.quan.109@gmail.com>
2012-06-18 14:32:34 +0800

    Signed-off-by: bofei <zhang.quan.109@gmail.com>

--- tool/common/header.php
+++ tool/common/header.php
@@ -3,45 +3,11 @@
 <head>
     <meta charset="gbk">
     <title>测试用例列表</title>
-    <link rel="stylesheet" href="../css/common.css" />
+    <link rel="stylesheet" href="common.css" />
     <style>
-        body{
-            margin:0;
-            padding:0;
-        }
 
-        ul{
-            list-style:none;
-        }
 
-        #header{
-            background:#FAFAFA;
-            background:-moz-linear-gradient(#FAFAFA, #EAEAEA);
-            background:-webkit-linear-gradient(#FAFAFA, #EAEAEA);
-            -ms-filter:"progid:DXImageTransform.Microsoft.gradient(startColorstr='#fafafa',endColorstr='#eaeaea')";
-            position:relative;
-            z-index:10;
-            border-bottom:1px solid #CACACA;
-            box-shadow:0 1px 0 rgba(255, 255, 255, 0.4), 0 0 10px rgba(0, 0, 0, 0.1);
-            height:40px;
-            line-height:40px;
-            text-align:center;
-        }
-        .header-wrap{
-             width:950px;
-            position:relative;
-            margin:auto;
-            text-align:left;
 
-        }
-        #header .logo{
-
-            font:62.5% "Droid Sans", Arial, sans-serif;
-            font-size:25px;
-            font-weight:bold;
-            height:40px;
-            line-height:40px;
-        }
     </style>
 </head>
 <body>
