f0438d8dcad62ff7996377bc099adac13b0dfa72
bofei <bofei.zq@taobao.com>
2012-07-08 15:26:51 +0800

    Signed-off-by: bofei <bofei.zq@taobao.com>

--- tool/common/header.php
+++ tool/common/header.php
@@ -5,8 +5,24 @@
     <title>测试用例列表</title>
     <link rel="stylesheet" href="http://uitest.taobao.net/tool/css/common.css" />
     <style>
+        #header ul{
+            position:absolute;
+            top:0;
+            right:0;
 
-
+        }
+        #header ul li{
+            display:inline-block;
+            margin-right:10px;
+        }
+        #header ul li a{
+            font-weight: bold;
+            color: #222;
+            -moz-transition: color .15s ease-in 0;
+            -webkit-transition: color .15s ease-in 0;
+            text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+            font-size:13px;
+        }
 
     </style>
 </head>
@@ -16,7 +32,9 @@
         <div class="header-wrap">
         <a href="/" class="logo">UITester</a>
         <ul>
-            <li></li>
+            <li><a href="http://uitest.taobao.net/tool/record/record.html?">录制用例</a></li>
+            <li><a href="http://uitest.taobao.net/tool/list.php">查看用例</a></li>
+            <li><a href="https://github.com/taobao-sns-fed/uitest/">测试框架</a></li>
         </ul>
         </div>
     </div>
