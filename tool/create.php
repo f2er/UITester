9a75dffab2cc2010a6f668f62de8be5897b731c0
bofei <bofei.zq@taobao.com>
2012-12-25 18:36:07 +0800

    UADAUPTE

--- tool/create.php
+++ tool/create.php
@@ -2,19 +2,80 @@
 
 <link rel="stylesheet" href="assets/form.css">
 
-
+<?php
+$task_name = '';
+$task_inject_uri = '';
+$modify_tag = '';
+?>
 <div class="container">
+    <div class="page-header">
+        <h1>创建新任务</h1>
+    </div>
 
-<h1>创建新任务</h1>
 
-<form method="POST" action="handle.php">
-	<table class="add-case-table">
-		<colgroup>
-			<col class="property" />
-			<col class="value" />
-		</colgroup>
+    <form class="form-horizontal" method="POST" action="handle.php">
+        <div class="control-group">
+            <label class="control-label" for="productline">产品</label>
+            <div class="controls">
+                <select id="productline" name="productline" required value="">
+                    <option value="" selected="selected">全部</option>
+                </select>
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="task_name">名称</label>
+            <div class="controls">
+                <input type="text" name="task_name" class="input-box" required/>
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="task_inject_uri">脚本地址</label>
+            <div class="controls">
+                <input type="text" name="task_inject_uri" class="input-box" required/>
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="timers">回归时间</label>
+            <div class="controls">
+                <input type="hidden" name="week">
+                <input type="checkbox" name="timers" value="1" />星期一
+                <input type="checkbox" name="timers" value="2" />星期二
+                <input type="checkbox" name="timers" value="3" />星期三
+                <input type="checkbox" name="timers" value="4" />星期四
+                <input type="checkbox" name="timers" value="5" />星期五
+                <input type="checkbox" name="timers" value="6" />星期六
+                <input type="checkbox" name="timers" value="7" />星期日
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="duration">回归频率</label>
+            <div class="controls">
+                每天回归的时间点: <input class="duration" type="text" name="start" value="" size="5" style="width:40px" placeholder="HH:mm">
+
+                回归频率: <select class="duration" name="duration">
+                <option value="30">0.5</option>
+                <option value="60">1</option>
+                <option value="120">2</option>
+                <option value="240">4</option>
+                <option value="480">8</option>
+                <option value="720">12</option>
+            </select>小时
+            </div>
+        </div>
+        <div class="control-group">
+            <div class="controls">
+                <input type="hidden" name="modify_tag" value="<?php echo $modify_tag ?>" />
+                <input type="hidden" name="task_id" value="<?php echo$task_id ?>" />
+                <button type="submit" class="btn">Sign in</button>
+            </div>
+        </div>
+
+
+
+
 
 		<?php
+        /*
 			$task_name = '';
 			$task_inject_uri = '';
 			$modify_tag = '';
@@ -65,6 +126,7 @@
 						</select>小时
 					</td>
 				</tr>
+
 				<tr>
 					<td colspan="2">
 						<input type="hidden" name="modify_tag" value="' . $modify_tag . '" />
@@ -73,8 +135,10 @@
 					</td>
 				</tr>
 			');
+        */
 		?>
 	</table>
+
 </form>
 
 <?php include_once('./common/footer.php'); ?>
