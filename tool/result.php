2342ff486d50035a66b6dfbe328d66b092133471
bofei <bofei.zq@taobao.com>
2012-12-26 19:53:22 +0800

    uapte

--- tool/result.php
+++ tool/result.php
@@ -53,7 +53,7 @@
         <span style="line-height: 20px; font-size: 24.5px;font-weight: bold"><?php echo $result_item['task_name'] ?></span>
 
         <div style="float: right">
-            <a id="go_test" target="_blank" class="btn btn-small" href="apply.php?id=<?php echo$result_item['id'] ?>">立即测试</a>
+            <a id="go_test" target="_blank" class="btn btn-small" href="#">立即测试</a>
             <a class="btn btn-small" href="modify.php?id=<?php $result_item['id'] ?>">修改</a>
             <a target="_self" class="btn btn-small" href="handle.php?modify_tag=remove&task_id=<?php echo $result_item['id'] ?>">删除</a>
         </div>
@@ -89,7 +89,20 @@
         <div class="tab-pane" id="ie7"></div>
         <div class="tab-pane" id="ie6"></div>
     </div>
+    <!-- Modal -->
+    <div id="myModal" style="width: 700px" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            <h3 id="myModalLabel">测试结果</h3>
+        </div>
+        <div class="modal-body result-report">
 
+        </div>
+        <div class="modal-footer">
+            <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+
+        </div>
+    </div>
     <?php include_once('./common/footer.php'); ?>
     <script>
         $('#myTab a').click(function (e) {
@@ -109,6 +122,74 @@
                 jsonReporter.renderHTML(value, jQuery("#" + key));
             });
         });
+    </script>
+
+    <script>
+        (function () {
+            var isConnected = false;
+            var http_host = location.host || "localhost";
+            var url = 'http://' + http_host + ":3030" + "/socket.io/socket.io.js"
+
+
+            jQuery.getScript(url, function () {
+
+                var socket = io.connect('http://' + http_host + ":3030");
+
+                if (!window.console) {
+                    window.console = {
+                        log:function () {
+                        },
+                        info:function () {
+                        },
+                        debug:function () {
+                        }
+                    }
+                }
+
+
+                jQuery("#go_test").on("click", function () {
+                    if (isConnected) {
+                        var src = jQuery("#test_src").val();
+
+                        jQuery("#myModal .modal-body").html("正在测试....");
+                        $('#myModal').modal('show');
+                        socket.emit("remote:task_start", {"task_inject_uri":<?php echo $task_inject_uri  ?>})
+                    }
+                    else {
+                        alert("未能成功连接")
+                    }
+
+                })
+
+                socket.on("connect", function () {
+                    console.info("connect");
+                    if (isConnected)return;
+                    isConnected = true;
+                    var jsonReporter = new jasmine.JsonReporter();
+                    $('#myModal').modal({show:false})
+                    socket.on("remote:task_finish", function (data) {
+                        $('#myModal').modal('show');
+                        for(var p in data.task_result){
+                            console.log(data.task_result[p])
+                            jQuery("#myModal .modal-body").html("")
+                            jsonReporter.renderHTML(data.task_result[p], jQuery("#myModal .modal-body"));
+                        }
+
+
+                        console.log("remote:task_finish", data)
+                    })
+
+
+                    // Send task report to server through Socket.IO
+                    // by trigger method of emit of socket object
+                    // socket.emit('console:task_finish', jasmineData);
+                })
+
+
+            })
+
+
+        })();
     </script>
 
 
