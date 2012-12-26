9c1bdf88ddd31779516ac8d688602b24d6d2518a
bofei <bofei.zq@taobao.com>
2012-12-26 21:31:29 +0800

    uapte

--- tool/local-test.php
+++ tool/local-test.php
@@ -25,12 +25,23 @@
 
     </div>
 
-    <div class="result-report">
+    <div id="myModal" style="width: 700px" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">¡Á</button>
+            <h3 id="myModalLabel">²âÊÔ½á¹û</h3>
+        </div>
+        <div class="modal-body result-report">
 
+        </div>
+        <div class="modal-footer">
+            <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+
+        </div>
     </div>
 
 
     <script>
+        $('#myModal').modal({show:false})
         $("#run_test").on("click", function () {
             jasmine._newEnv = true;
             UT.configs.windowType = "window";
@@ -39,7 +50,9 @@
                 $.getScript(value, function(){
                     UT.execute(function (data) {
                         var jsonReporter = new jasmine.JsonReporter();
-                        jsonReporter.renderHTML(data, jQuery(".result-report"));
+                        jQuery("#myModal .modal-body").html("")
+                        jsonReporter.renderHTML(data.task_result[p], jQuery("#myModal .modal-body"));
+                        $('#myModal').modal('show');
                     })
                 })
             }
@@ -47,7 +60,9 @@
                 eval(value);
                 UT.execute(function (data) {
                     var jsonReporter = new jasmine.JsonReporter();
-                    jsonReporter.renderHTML(data, jQuery(".result-report"));
+                    jQuery("#myModal .modal-body").html("")
+                    jsonReporter.renderHTML(data.task_result[p], jQuery("#myModal .modal-body"));
+                    $('#myModal').modal('show');
                 })
             }
 
