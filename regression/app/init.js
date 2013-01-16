e6913d5d59fdf12881d7f0c9dd9550569270d71f
seavers <lianghaijun001@163.com>
2013-01-16 11:23:09 +0800

    在日志中加入时间

--- regression/app/init.js
+++ regression/app/init.js
@@ -1,22 +1,45 @@
 function start () {
-    console.log('Mother process is running.');
+    console.log(time() + ' [INFO] ' + 'Mother process is running.');
 
     var ls = require('child_process').spawn('node', ['server.js']);
 
     ls.stdout.on('data', function (data) {
-        console.log(data.toString());
+		data.toString().split('\n').forEach(function(line) {
+			line && console.log(time() + ' [INFO] ' + line);
+		});
     });
 
     ls.stderr.on('data', function (data) {
-        console.log(data.toString());
+		data.toString().split('\n').forEach(function(line) {
+	        line && console.log(time() + ' [ERROR] ' + line);
+		});
     });
 
     ls.on('exit', function (code) {
-        console.log('child process exited with code ' + code);
+        console.log(time() + ' [ERROR] ' + 'child process exited with code ' + code);
         delete(ls);
         setTimeout(start, 3000);
     });
 
 };
+
+function time() {
+	var date = new Date();
+	return '' + 
+			(date.getYear() + 1900) + '-' + 
+			padding(date.getMonth()+1) + '-' + 
+			padding(date.getDate()) + ' ' + 
+			padding(date.getHours()) + ':' + 
+			padding(date.getMinutes()) + ':' + 
+			padding(date.getSeconds()) + '.' + 
+			padding(date.getMilliseconds(), 3)
+}
+function padding(num, count) {
+	if (count) {
+		return num > 99 ? num : '0' + (num > 9 ? num : '0' + num);
+	} else {
+		return num > 9 ? num : '0' + num;
+	}
+}
 
 start();
