167490567fcb1d0828b67825f4d3555312f4b584
zhangquan <zhang.quan.app@gmail.com>
2013-03-14 14:46:37 +0800

    udpate
    update

--- tool/assets/result-html.js
+++ tool/assets/result-html.js
@@ -1,6 +1,6 @@
-var renderOne = function(result ,el){
+var renderOne = function (result, el) {
     var r = result;
-    var msg="";
+    var msg = "";
     msg += '<div class="browser">' + r.browser + '</div>';
     var passed = "passed";
     if (r.failedSpecs != 0 || (r.errors && r.errors.length)) {
@@ -40,7 +40,7 @@ var renderOne = function(result ,el){
         }
         for (var j = 0; j < url.suites.length; i++) {
 
-            var suite = url.suites.length;
+            var suite = url.suites[j];
 
             msg += '<span class="suite-title">' + suite.description + '</span>';
             for (var n = 0; n < suite.specs.length; n++) {
@@ -52,18 +52,17 @@ var renderOne = function(result ,el){
                 msg += '<div class="spec ' + passed + '">';
                 msg += '<a class="spec-title" href="#">' + spec.description + '</a>' +
                     '<div class="detail">';
-
-                spec.results_.forEach(function (result) {
-                    result.items_.forEach(function (item) {
-                        msg += '<p>';
-                        msg += 'expect ' + item.actual + ' ' + item.matcherName + ' ' + item.expected + ' ' + item.passed_ == true ? item.message : "failed";
-                        msg += '<div class="error-stack">${item.trace.stack}</div>'
-                        msg += '</p>';
-
-
+                for (var m = 0; m < spec.results_.length; m++)
+                    spec.results_.forEach(function (result) {
+                        var result = spec.results_[m];
+                        for (var p = 0; p < result.items_.length; p++) {
+                            var item = result.items_[p];
+                            msg += '<p>';
+                            msg += 'expect ' + item.actual + ' ' + item.matcherName + ' ' + item.expected + ' ' + item.passed_ == true ? item.message : "failed";
+                            msg += '<div class="error-stack">${item.trace.stack}</div>'
+                            msg += '</p>';
+                        }
                     })
-
-                })
                 msg += "</div>"
                 msg += "</div>"
 
@@ -90,7 +89,7 @@ var renderResult = function (result, el) {
     var msg = "";
     for (var b in result) {
         var r = result[b];
-      msg+= renderOne(r);
+        msg += renderOne(r);
 
 
     }
